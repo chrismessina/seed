a36497be13547fe81f2ca212a558be6c5301e291
Ross Moody <fiftyfivehis@gmail.com>
2019-08-12 13:59:57 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// import icons from './icons';
+// import toSvg from './to-svg';
+// import replace from './replace';
+
+// module.exports = { icons, toSvg, replace };
+
+(function testFunc() {
+  console.log('Testing testing');
+})();
