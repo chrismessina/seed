c51200e590f44dab39929bda85e4baf6459e66fb
Ross Moody <fiftyfivehis@gmail.com>
2019-08-13 12:48:14 -0700

    more cleanup

--- src/replace.js
+++ src/replace.js
@@ -2,17 +2,11 @@ import classnames from 'classnames/dedupe'
 
 import icons from './icons'
 
-/**
- * Replace all HTML elements that have a `data-seed` attribute with SVG markup
- * corresponding to the element's `data-seed` attribute value.
- * @param {Object} attrs
- */
 function replace(attrs = {}) {
   if (typeof document === 'undefined') {
     throw new Error('`seed.replace()` only works in a browser environment.')
   }
 
-  // eslint-disable-next-line no-undef
   const elementsToReplace = document.querySelectorAll('[data-seed]')
 
   Array.from(elementsToReplace).forEach(element =>
@@ -20,12 +14,6 @@ function replace(attrs = {}) {
   )
 }
 
-/**
- * Replace a single HTML element with SVG markup
- * corresponding to the element's `data-seed` attribute value.
- * @param {HTMLElement} element
- * @param {Object} attrs
- */
 function replaceElement(element, attrs = {}) {
   const elementAttrs = getAttrs(element)
   const name = elementAttrs['data-seed']
@@ -36,21 +24,13 @@ function replaceElement(element, attrs = {}) {
     ...elementAttrs,
     ...{ class: classnames(attrs.class, elementAttrs.class) }
   })
-  // eslint-disable-next-line no-undef
-  const svgDocument = new DOMParser().parseFromString(
-    svgString,
-    'image/svg+xml'
-  )
+
+  const svgDocument = new DOMParser().parseFromString(svgString, 'text/html')
   const svgElement = svgDocument.querySelector('svg')
 
   element.parentNode.replaceChild(svgElement, element)
 }
 
-/**
- * Get the attributes of an HTML element.
- * @param {HTMLElement} element
- * @returns {Object}
- */
 function getAttrs(element) {
   return Array.from(element.attributes).reduce((attrs, attr) => {
     attrs[attr.name] = attr.value
