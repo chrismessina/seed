20104b92786f8a6c546cf70e5696fdd6db4342bb
rossmoody <fiftyfivehis@gmail.com>
2019-08-21 11:11:00 -0700

    fixes

--- package/icons.js
+++ package/icons.js
@@ -2,7 +2,7 @@ import Icon from './icon'
 import icons from '../dist/icons.json'
 
 export default Object.keys(icons)
-  .map(key => new Icon(key, icons[key], tags[key]))
+  .map(key => new Icon(key, icons[key]))
   .reduce((object, icon) => {
     object[icon.name] = icon
 
