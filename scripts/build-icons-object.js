d1674ae51ce8c3471560afe902ed6a1dad939168
rossmoody <fiftyfivehis@gmail.com>
2019-08-21 21:19:52 -0700

    hi

--- scripts/build-icons-object.js
+++ scripts/build-icons-object.js
@@ -3,22 +3,22 @@ import cheerio from 'cheerio'
 import { minify } from 'html-minifier'
 
 function buildIconsObject(svgFiles, getSvg) {
-  return svgFiles
-    .map(svgFile => {
-      const name = path.basename(svgFile, '.svg')
-      const svg = getSvg(svgFile)
-      const contents = getSvgContents(svg)
-      return { name, contents }
-    })
-    .reduce((icons, icon) => {
-      icons[icon.name] = icon.contents
-      return icons
-    }, {})
+    return svgFiles
+        .map(svgFile => {
+            const name = path.basename(svgFile, '.svg')
+            const svg = getSvg(svgFile)
+            const contents = getSvgContents(svg)
+            return { name, contents }
+        })
+        .reduce((icons, icon) => {
+            icons[icon.name] = icon.contents
+            return icons
+        }, {})
 }
 
 function getSvgContents(svg) {
-  const $ = cheerio.load(svg)
-  return minify($('svg').html(), { collapseWhitespace: true })
+    const $ = cheerio.load(svg)
+    return minify($('svg').html(), { collapseWhitespace: true })
 }
 
 export default buildIconsObject
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,26 @@
 const path = require('path')
 
 module.exports = {
-  entry: ['core-js/es/array/from', path.resolve(__dirname, 'package/index.js')],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    libraryTarget: 'umd',
-    library: 'seed',
-    globalObject: "typeof self !== 'undefined' ? self : this"
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      }
-    ]
-  }
+    entry: path.resolve(__dirname, 'package/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        libraryTarget: 'umd',
+        library: 'seed',
+        globalObject: "typeof self !== 'undefined' ? self : this",
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
 }
