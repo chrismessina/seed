0d3a34bf5eb8b17c6a83a707c57a662308906606
Ross Moody <fiftyfivehis@gmail.com>
2020-06-11 12:39:32 -0700

    usage

--- seedicons.com/src/components/usage/index.js
+++ seedicons.com/src/components/usage/index.js
@@ -1,48 +1,12 @@
 import React from 'react'
 import { PageCont, Content } from './styled'
+import MDX from './usage'
 
 export const Usage = () => {
   return (
     <PageCont>
       <Content>
-        <h1>Heading 1</h1>
-        <p>
-          Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh
-          onion daikon amaranth tatsoi tomatillo melon azuki bean garlic. Gumbo
-          beet greens corn soko endive gumbo gourd. Parsley shallot courgette
-          tatsoi pea sprouts fava bean collard greens dandelion okra wakame
-          tomato. Dandelion cucumber earthnut pea peanut soko zucchini. Turnip
-          greens yarrow ricebean rutabaga endive cauliflower sea lettuce
-          kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus
-          winter purslane kale. Celery potato scallion desert raisin horseradish
-          spinach carrot soko. Lotus root water spinach fennel kombu maize
-          bamboo shoot green bean swiss chard seakale pumpkin onion chickpea
-          gram corn pea. Brussels sprout coriander water chestnut gourd swiss
-          chard wakame kohlrabi beetroot carrot watercress. Corn amaranth
-          salsify bunya nuts nori azuki bean chickweed potato bell pepper
-          artichoke.
-        </p>
-        <h2>Heading 2</h2>
-        <p>
-          Veggies es bonus vobis, <a>this is a link it goes nowheres</a>proinde
-          vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi
-          tomatillo melon azuki bean garlic. Gumbo beet greens corn soko endive
-          gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean
-          collard greens dandelion okra wakame tomato. Dandelion cucumber
-          earthnut pea peanut soko zucchini.{' '}
-          <code>Testing testing one two</code>Turnip greens yarrow ricebean
-          rutabaga endive cauliflower sea lettuce kohlrabi amaranth water
-          spinach avocado daikon napa cabbage asparagus winter purslane kale.
-          Celery potato scallion desert raisin horseradish spinach carrot soko.
-          Lotus root water spinach fennel kombu maize bamboo shoot green bean
-          swiss chard seakale pumpkin onion chickpea gram corn pea. Brussels
-          sprout coriander water chestnut gourd swiss chard wakame kohlrabi
-          beetroot carrot watercress. Corn amaranth salsify bunya nuts nori
-          azuki bean chickweed potato bell pepper artichoke.
-        </p>
-        <p className='styled-code'>
-          <pre><ion-icon ios="heart-outline" md="heart-sharp"></ion-icon></pre>
-        </p>
+        <MDX />
       </Content>
     </PageCont>
   )
--- seedicons.com/src/components/usage/styled.js
+++ seedicons.com/src/components/usage/styled.js
@@ -1,118 +1,4 @@
-import styled, { css } from 'styled-components'
-
-const globalStyles = css`
-  font-style: normal;
-  font-size: 15px;
-  font-weight: 400;
-  line-height: 1.6;
-  margin: 0px;
-
-  h1,
-  h2,
-  h3,
-  h4,
-  h5 {
-    color: ${(p) => p.theme.colors.header};
-  }
-  h1 {
-    font-size: 36px;
-    line-height: 44px;
-    font-weight: 700;
-    letter-spacing: -0.03em;
-    color: ${(p) => p.theme.colors.header};
-  }
-  h2 {
-    font-size: 24px;
-    margin-top: 60px;
-  }
-  h2,
-  h3 {
-    margin-bottom: 0;
-    letter-spacing: -0.03em;
-  }
-  h3 {
-    font-size: 18px;
-    line-height: 28px;
-    margin-top: 40px;
-  }
-  p {
-    font-size: 15px;
-    line-height: 28px;
-    margin-top: 10px;
-    letter-spacing: 0;
-    color: ${(p) => p.theme.colors.text};
-  }
-  p > a {
-    -webkit-transition: border-color 0.3s;
-    transition: border-color 0.3s;
-    text-decoration: none;
-    color: ${(p) => p.theme.colors.primary};
-    border-bottom: 1px solid transparent;
-    cursor: pointer;
-  }
-  p > a:hover {
-    border-bottom-color: var(--color-dodger-blue-light);
-  }
-  .hljs-comment {
-        color: #998;
-        font-style: italic;
-    }
-    .hljs-selector-tag {
-        color: #333;
-    }
-    .hljs-number,
-    .hljs-tag .hljs-attr {
-        color: teal;
-    }
-    .hljs-string {
-        color: #d14;
-    }
-    .hljs-attribute,
-    .hljs-name,
-    .hljs-tag {
-        color: navy;
-        font-weight: 400;
-    }
-    code,
-    pre {
-        font-family: Roboto Mono, monospace;
-    }
-    code {
-        background-color: var(--color-bright-gray);
-        color: #97a1b6;
-        padding: 8px 12px;
-        border-radius: 5px;
-    }
-    p code {
-        font-size: 13px;
-        font-weight: 500;
-        padding: 2px 5px;
-        color: #f06332;
-    }
-    .styled-code,
-    p code {
-        background-color: #f5f7fa;
-    }
-    .styled-code {
-        -webkit-box-sizing: border-box;
-        box-sizing: border-box;
-        padding: 16px 14px;
-        width: 100%;
-        border-radius: 5px;
-        display: block;
-        overflow-x: auto;
-        margin: 18px 0;
-    }
-    .styled-code + .styled-code {
-        margin-top: 12px;
-    }
-    pre {
-        margin: 0;
-        font-size: 14px;
-        line-height: 20px;
-        color: var(--color-pale-sky);
-    }
-`
+import styled from 'styled-components'
 
 export const PageCont = styled.div`
   max-width: ${(p) => p.theme.breakpoints.medium};
@@ -121,5 +7,97 @@ export const PageCont = styled.div`
 `
 
 export const Content = styled.div`
-  ${globalStyles}
+  font-style: normal;
+  font-size: 15px;
+  font-weight: 400;
+  line-height: 1.6;
+  margin: 0px;
+
+  h1 {
+  font-size: 36px;
+  line-height: 44px;
+  font-weight: 700;
+  letter-spacing: -0.03em;
+  color: ${p=>p.theme.colors.header};
+}
+
+h2 {
+  font-size: 24px;
+  margin-top: 60px;
+  margin-bottom: 0;
+  letter-spacing: -0.03em;
+  color: ${p=>p.theme.colors.header};
+}
+
+h3 {
+  font-size: 18px;
+  line-height: 28px;
+  margin-top: 40px;
+  margin-bottom: 0;
+  letter-spacing: -0.03em;
+  color: ${p=>p.theme.colors.header};
+}
+
+h4 {
+  font-size: 16px;
+  line-height: 16px;
+  font-weight: 600;
+  color: ${p=>p.theme.colors.header};
+}
+
+h6 {
+  font-size: 10px;
+  line-height: 10px;
+  font-weight: 600;
+  text-transform: uppercase;
+  letter-spacing: 0.05em;
+  color: var(--color-heather);
+  margin: 0;
+}
+
+p {
+  color: ${p=>p.theme.colors.text};
+  font-size: 14px;
+  line-height: 28px;
+  margin-top: 10px;
+  letter-spacing: 0;
+}
+
+p > a {
+  transition: border-color 0.3s;
+  text-decoration: none;
+  color: ${p=>p.theme.colors.primary};
+  border-bottom: 1px solid transparent;
+}
+
+p > a:hover {
+  border-bottom-color: ${p=>p.theme.colors.primary};
+}
+
+code,
+pre {
+  font-family: 'Roboto Mono', monospace;
+}
+
+code {
+  background-color: ${p=>p.theme.colors.stroke};
+  color: #97a1b6;
+  padding: 8px 12px;
+  border-radius: 5px;
+}
+
+p > code {
+  font-size: 13px;
+  font-weight: 500;
+  padding: 2px 5px;
+  color: #f06332;
+  background-color: #f5f7fa;
+}
+
+pre {
+  margin: 0;
+  font-size: 14px;
+  line-height: 20px;
+  color: ${p=>p.theme.colors.stroke};
+}
 `
